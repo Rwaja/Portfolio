fbdc265ffa7cdba9354b6b91ea2227689cb8b008
Rodrigo Huajamaita <rhuajamaita@gmail.com>
2023-11-18 15:03:42 -0300

    Refactor code structure and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,19 +5,21 @@ export default function Navbar() {
     <motion.nav
       style={{
         position: "fixed",
+        top: 0,
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "end",
         alignItems: "center",
         backgroundColor: "black",
         color: "white",
-        height: "10vh",
+        height: "8vh",
         width: "100%",
         zIndex: 100,
       }}
     >
-      <a href="#about" style={{ width: '15vh', height:'100%' }}>nav</a>
-      <a href="#skills" style={{ width: '15vh', height:'100%' }}>nav</a>
-      <a href="#proyects" style={{ width: '15vh', height:'100%' }}>nav</a>
+      <a href="#about" style={{ width: '15vh', height:'100%' }}>About</a>
+      <a href="#skills" style={{ width: '15vh', height:'100%' }}>Skills</a>
+      <a href="#proyects" style={{ width: '15vh', height:'100%' }}>Proyects</a>
+      <a href="#contact" style={{ width: '15vh', height:'100%' }}>Contact</a>
     </motion.nav>
   );
 }
--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -12,30 +12,14 @@ interface Props {
   children: React.ReactNode;
 }
 export default function Return({ children }: Props) {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-  const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [0.1, 1]);
-  const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
-  const y = useTransform(scrollYProgress, [0, 1], [75, 0]);
-  const mainControls = useAnimation();
-  const slideControls = useAnimation();
-  useEffect(() => {
-    if (isInView) {
-      mainControls.start("visible");
-      slideControls.start("visible");
-    }
-  }, [isInView]);
-
+ 
   return (
-    <div>
       <motion.div
-        initial={{ opacity: 0.2, scale: 0.8 }}
-        transition={{ duration: 0.1, ease: "linear"}}
-        whileInView={{ opacity: 1, scale: 1 }}
+        initial={{ opacity: 0.2} }
+        transition={{ duration: 0.7}}
+        whileInView={{ opacity: 1 }}
       >
         {children}
       </motion.div>
-    </div>
   );
 }
