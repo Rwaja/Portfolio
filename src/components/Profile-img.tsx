59b6ea1c8bb8e5627e84ee8df5c899423f9d33b9
Rodrigo Huajamaita <rhuajamaita@gmail.com>
2023-12-13 13:48:28 -0300

    Update Main.astro with TODOs and remove unused components

--- src/components/Profile-img.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProfileImg() {
-  return (
-    <img
-      src="/green-color-solid-background-1920x1080.png"
-      alt="background"
-      style={{ width: "100px", height: "100px" }}
-    />
-  );
-}
