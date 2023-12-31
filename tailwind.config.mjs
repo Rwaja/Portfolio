1889ce5b996ac8b66dd04c6d8af2099d4dd1ac46
Rodrigo Huajamaita <rhuajamaita@gmail.com>
2023-12-31 17:07:13 -0300

    Add Separator component and update styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'border-color': 'hsl(214, 31.8%, 91.4%)',
+			  },
+		},
 	},
 	plugins: [],
 }
