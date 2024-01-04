f498cb94f1285cee247fc35af5aed9b2cf023ce0
Rodrigo Huajamaita <rhuajamaita@gmail.com>
2024-01-03 21:53:40 -0300

    Update styles and add hover effects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,9 @@ export default {
 				'border-color': 'hsl(240, 3.7%, 15.9%)',
 				'background-color': 'hsl(240 10% 3.9%)',
 			  },
+			  boxShadow: {
+				'blue': '0 4px 14px 0 rgba(0, 118, 255, 0.39)',
+			  }
 		},
 	},
 	plugins: [],
